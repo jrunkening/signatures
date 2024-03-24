8ab0314eb6b6a387363314ed3221dceb5d9a20bb
JingYu Ning <115336923+jrunkening@users.noreply.github.com>
2024-03-24 18:51:52 +0800

    Implement cot laplacian

--- src/signatures/laplace.py
+++ src/signatures/laplace.py
@@ -15,11 +15,45 @@ def laplacian_beltrami(mesh: trimesh.Trimesh):
     n = len(mesh.vertices)
     indices = torch.hstack([
         torch.tensor(mesh.edges).transpose(0, 1), # for every (i, j) in `mesh.edges`
-        torch.vstack([torch.arange(n).unsqueeze(0), torch.arange(n).unsqueeze(0)]), # for every (i, j) where i==j
+        torch.arange(n).unsqueeze(0).expand(2, -1), # for every (i, j) where i==j
     ])
     values = torch.hstack([
         -torch.ones(len(mesh.edges)), # -1
         torch.tensor(mesh.vertex_degree), # deg(mesh.vertices[i])
     ])
+
+    return torch.sparse_coo_tensor(indices, values)
+
+
+def cotan(vector0, vector1):
+    return torch.sum(vector0*vector1) / torch.linalg.cross(vector0, vector1).square().sum(dim=-1).sqrt()
+
+
+def laplacian_cotangens(mesh: trimesh.Trimesh):
+    r"""
+        idx vtx
+         a   l
+        / \ / \
+        i-j u-v
+        \ / \ /
+         b   r
+    """
+    n = len(mesh.vertices)
+
+    ij = mesh.face_adjacency_edges # (#edges_shared, 2)
+    ab = mesh.face_adjacency_unshared # (#edges_unshared, 2)
+
+    uv = torch.tensor(mesh.vertices[ij]) # (#edges_shared, 2, 3)
+    lr = torch.tensor(mesh.vertices[ab]) # (#edges_unshared, 2, 3)
+
+    ca = cotan(lr[:, 0] - uv[:, 0], lr[:, 0] - uv[:, 1])
+    cb = cotan(lr[:, 1] - uv[:, 0], lr[:, 1] - uv[:, 1])
+
+    w_ij = (0.5 * (ca + cb)).unsqueeze(1)
+
+    indices = torch.empty((2, 4*len(ij)), dtype=torch.long)
+    for index, (i, j) in enumerate(ij):
+        indices[:, (index*4):(index*4+4)] = torch.tensor([[i, j, i, j], [j, i, i, j]])
+    values = torch.hstack([-w_ij, -w_ij, w_ij, w_ij]).reshape(-1)
 
     return torch.sparse_coo_tensor(indices, values)
--- test/test_signatures/test_laplace.py
+++ test/test_signatures/test_laplace.py
@@ -1,6 +1,7 @@
 import unittest
 import trimesh
 import torch
+import numpy
 
 import prototype.laplace as laplace_proto
 
@@ -29,4 +30,22 @@ class LaplaceTestCase(unittest.TestCase):
         self.assert_close(
             laplace.laplacian_beltrami(self.mesh),
             laplace_proto.build_laplace_beltrami_matrix(self.mesh)
+        )
+
+    def test_cotan(self):
+        n = 5
+
+        vector0 = torch.rand(n, 3)
+        vector1 = torch.rand(n, 3)
+
+        actual = laplace.cotan(vector0, vector1)
+        expected = torch.tensor(
+            numpy.sum(vector0.numpy()*vector1.numpy()) / numpy.linalg.norm(numpy.cross(vector0.numpy(), vector1.numpy()), axis=-1)
+        )
+        torch.testing.assert_close(actual, expected)
+
+    def test_laplacian_beltrami(self):
+        self.assert_close(
+            laplace.laplacian_cotangens(self.mesh),
+            laplace_proto.build_cotangens_matrix(self.mesh)
         )
