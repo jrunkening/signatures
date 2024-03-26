4c5a97755f96bb5d978dcfc76503f0bc8b534f4c
JingYu Ning <115336923+jrunkening@users.noreply.github.com>
2024-03-26 15:21:20 +0800

    Implement wks

--- src/signatures/signatures.py
+++ src/signatures/signatures.py
@@ -6,6 +6,20 @@ import trimesh
 from signatures.laplace import mass_matrix, laplacian_beltrami
 
 
+def wave_kernel_signatures(
+    mesh: trimesh.Trimesh,
+    n_basis: int, n_energy_steps: int, e_min: float = None, e_max: float = None,
+    laplacian = laplacian_beltrami,
+    device = "cpu"
+):
+    n_basis = min(len(mesh.vertices)-1, n_basis)
+
+    return wks(
+        laplacian(mesh).to(device), mass_matrix(mesh).to(device),
+        n_basis, n_energy_steps, e_min, e_max
+    )
+
+
 def heat_kernel_signatures(
     mesh: trimesh.Trimesh,
     n_basis: int, n_time_steps: int, t_min: float = None, t_max: float = None,
@@ -18,6 +32,32 @@ def heat_kernel_signatures(
         laplacian(mesh).to(device), mass_matrix(mesh).to(device),
         n_basis, n_time_steps, t_min, t_max
     )
+
+
+def wks(
+    laplacian_mesh: torch.tensor, mass_matrix: torch.tensor,
+    n_basis: int, n_energy_steps: int, e_min: float = None, e_max: float = None,
+):
+    assert mass_matrix.device == laplacian_mesh.device
+    device = laplacian_mesh.device
+
+    spectrum, modes = torch.lobpcg(laplacian_mesh, n_basis, mass_matrix) # (n_basis,), (#v, n_basis)
+
+    if e_min is None:
+        e_min = spectrum.min().log()
+    if e_max is None:
+        e_max = spectrum.max().log() / 1.02
+
+    energies = torch.linspace(e_min, e_max, n_energy_steps).to(device) # (n_time_steps,)
+
+    sigma = 7.0 * (energies[-1] - energies[0]) / n_energy_steps
+    phi2 = torch.square(modes) # (#v, n_basis)
+    exp = torch.exp(-energies.square()[None] - spectrum.log()[..., None]/(2.0 * sigma.square()))
+    signatures = torch.sum(phi2[..., None] * exp[None], dim=1)
+    energy_trace = torch.sum(exp, dim=0, keepdim=True) # (1, n_time_steps)
+    signatures = signatures / energy_trace
+
+    return signatures
 
 
 def hks(
